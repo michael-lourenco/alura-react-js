3ed8c6be468f0ef89dc22adc957a8e98361e0ec9
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-02-07 15:28:12 -0300

    Binds

--- ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,25 +1,43 @@
 import React, { Component } from "react";
 import "./estilo.css";
 class FormularioCadastro extends Component {
-  render() {
-    return (
-      <form className="form-cadastro ">
-        <input
-          type="text"
-          placeholder="Título"
-          className="form-cadastro_input"
-        />
-        <textarea
-          rows={15}
-          placeholder="Escreva sua nota..."
-          className="form-cadastro_input"
-        />
-        <button className="form-cadastro_input form-cadastro_submit">
-          Criar Nota
-        </button>
-      </form>
-    );
-  }
+
+    constructor() {
+        super()
+        this.titulo = "";
+        this.texto = "";
+    }
+
+    handlerMudancaTitulo = (event) => {
+        this.titulo = event.target.value;
+        console.log(this.titulo);
+    }
+
+    handlerMudancaTexto = (event) => {
+        this.texto = event.target.value;
+    }
+
+    render() {
+        return (
+        <form className="form-cadastro ">
+            <input
+            type = "text"
+            placeholder = "Título"
+            className = "form-cadastro_input"
+            onChange = { this.handlerMudancaTitulo.bind(this) }
+            />
+            <textarea
+            rows={15}
+            placeholder="Escreva sua nota..."
+            className="form-cadastro_input"
+            onChange = { this.handlerMudancaTexto.bind(this) }
+            />
+            <button className="form-cadastro_input form-cadastro_submit">
+            Criar Nota
+            </button>
+        </form>
+        );
+    }
 }
 
 export default FormularioCadastro;
