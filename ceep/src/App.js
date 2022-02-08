f92af1e614c7761102e11d51ce1d4283b950e7ab
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-02-08 16:52:54 -0300

    Criando lista de categorias

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import ListaDeNotas from "./components/ListaDeNotas";
 import FormularioCadastro from "./components/FormularioCadastro";
+import ListaDeCategorias from "./components/ListaDeCategorias";
 import "./assets/App.css";
 import './assets/index.css';
 class App extends Component {
@@ -28,12 +29,15 @@ class App extends Component {
 
   render() {
     return (
-      <section className="conteudo">
+      <section className = "conteudo">
         <FormularioCadastro criarNota = { this.criarNota.bind(this) }/>
-        <ListaDeNotas 
-          apagarNota = { this.apagarNota.bind(this) }
-          notas = { this.state.notas }
-        />
+        <main className = "conteudo-principal">
+          <ListaDeCategorias />
+          <ListaDeNotas 
+            apagarNota = { this.apagarNota.bind(this) }
+            notas = { this.state.notas }
+          />
+        </main>
       </section>
     );
   }
