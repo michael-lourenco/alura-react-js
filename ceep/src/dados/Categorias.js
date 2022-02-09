4c7fa1ad79948c5395c1f97bc2b6188103fc41bc
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-02-09 11:23:29 -0300

    Criando lista de inscritos

--- ceep/src/dados/Categorias.js
+++ ceep/src/dados/Categorias.js
@@ -1,9 +1,19 @@
 export default class Caterogias {
     constructor() {
         this.categorias = [];
+        this._inscritos = [];
     }
-    
+
     adicionarCategoria(novaCategoria) {
         this.categorias.push(novaCategoria);
     }
+
+    inscrever(func) {
+        this._inscritos.push(func);
+    }
+
+    notificar() {
+        this._inscritos.forEach(func => func(this.categorias));
+    }
+
 }
